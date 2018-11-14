0effbdd3e8b7e4ba86cbcc073be3954b2e8bd8bd
Mauricio Braz <aguiarbrazm@gmail.com>
2018-11-14 15:31:39 -0200

    Added Damage Class and Building Damageable class

---
+++ Ninja Assault/Assets/Scripts/Damager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+// A behaviour father to be implemented to differents Objects
+public class Damager :MonoBehaviour {
+
+    public float damage;
+
+}
